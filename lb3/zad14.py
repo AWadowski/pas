056a8292c1029d993e4a9883fdaf3e970d441f2a
Adam Wadowski <adam.wadowski@assecods.pl>
2023-03-17 09:47:55 +0100

    13/14

--- lb3/zad14.py
+++ lb3/zad14.py
@@ -1,7 +1,7 @@
 import socket
 
 
-datagram = "ed 74 0b 55 00 24 ef fd 70 72 6f 67 72 61 6d 6d 69 6e 67 20 69 6e 20 70 79 74 68 6f 6e 20 69 73 20 66 75 6e".split()
+datagram = "0b 54 89 8b 1f 9a 18 ec bb b1 64 f2 80 18 00 e3 67 71 00 00 01 01 08 0a 02 c1 a4 ee 00 1a 4c ee 68 65 6c 6c 6f 20 3a 29".split()
 source_port = int("".join(datagram[:2]), 16)
 dest_port = int("".join(datagram[2:4]), 16)
 length = int("".join(datagram[4:6]), 16)
@@ -9,12 +9,12 @@ checksum = int("".join(datagram[6:8]), 16)
 data = "".join([chr(int(x, 16)) for x in datagram[8:]])
 print(source_port)
 print(dest_port)
-print(length)
-print(checksum)
 print(data)
-odp = f"zad14odp;src;{source_port};dst;{dest_port};data;{data}"
+odp = f"zad13odp;src;{source_port};dst;{dest_port};data;{data}"
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-result_stream = client.connect_ex(('212.182.24.236', 2910))
+result_stream = client.connect_ex(('212.182.24.236', 2909))
+
 client.settimeout(1)
 client.send(odp.encode('utf-8'))
 result = client.recv(1024)
